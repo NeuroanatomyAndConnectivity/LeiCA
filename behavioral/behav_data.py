75acaeb4a9757cc527172727853a7213bc171aab
fliem <franz.liem>
2015-09-08 07:40:56 +0200

    moved utils to utils.py

--- behavioral/behav_data.py
+++ behavioral/behav_data.py
@@ -1,65 +1,45 @@
 #FIXME
-
 #check for duplicates
-
-
+import os, glob
 import pandas as pd
-import os
-
-def read_nki_cvs(filename):
-    '''
-    imports nki csv and returns pandas data frame with 'Anonymous ID' as index
-    csv files with header like:
-        "","","Calculated Age"
-        "Anonymized ID","Subject Type","AGE_04"
-        "A000355xx","Child-Age 11","11.183561643836"
-    '''
-
-    df = pd.read_csv(filename, header=1, index_col='Anonymized ID')
-    df['origin_file'] = os.path.basename(filename)
-    return df
-
-
-def merge_dataframes(df1, df2):
-    '''
-    returns merged data frame
-    with outer: also returns subjects that are only included in one df (with nans)
-    '''
-    return pd.merge(df1, df2, left_index=True, right_index=True, how='outer')
+from utils import read_nki_cvs, leica_id_to_a_number_mapping, merge_dataframes
 
 
 
-def leica_id_to_a_number_mapping(leica_subjects_file, nki_id_mapping_file):
-    '''
-    for release 1...5 nki subjects '010...' finds 'A...' number
-    for > r5 subjects
-    returns dataframe with index is leica_subject_id and a_number
-    '''
-    leica_subjects = pd.read_csv(leica_subjects_file, header=None, names=['leica_id'])
-    leica_subjects.set_index(leica_subjects.leica_id, inplace=True)
+behav_files_path = '/Users/franzliem/Dropbox/LeiCA/assessment_data_20150817/'
+behav_files_stubs_list =  ['Age_'] #['Demos_', '_Age_'] #, 'STAI']
 
-    nki_id_mapping = pd.read_csv(nki_id_mapping_file)
-    nki_id_mapping.set_index(nki_id_mapping.a_number, inplace=True)
-    nki_id_mapping['zero_number'] =  ['01'+ v[4:] for v in  nki_id_mapping.m_number.values]
-
-
-    for l in leica_subjects.index:
-        if l.startswith('A'): #use A number a
-            leica_subjects.loc[l,'a_number'] = l
-        else:
-            leica_subjects.loc[l,'a_number'] = \
-                nki_id_mapping.loc[nki_id_mapping.zero_number=='0161348', 'a_number'].index[0]
-
-    return leica_subjects
-
-
-
-
-leica_subjects_file = '/Users/franzliem/Dropbox/Workspace/LeiCA/subjects/subjects_2015-09-05_redo_metrics.txt'
+#preare subject name mapping
+leica_subjects_file = '/Users/franzliem/Dropbox/Workspace/LeiCA/subjects/subjects_2015-09-06_r1-7.txt'
 nki_id_mapping_file = '/Users/franzliem/Dropbox/LeiCA/nki_r1_r6_matched_ids_unq.csv'
 
-df1 = read_nki_cvs('/Users/franzliem/Dropbox/LeiCA/assessment_data_20150817/8100_Age_20150817.csv')
-df1.head()
+leica_subjects = leica_id_to_a_number_mapping(leica_subjects_file, nki_id_mapping_file)
 
-df2 = read_nki_cvs('/Users/franzliem/Dropbox/LeiCA/assessment_data_20150817/8100_Demos_20150817.csv')
-df1.head()
+
+df = pd.DataFrame([])
+
+for file_stub in behav_files_stubs_list:
+    file_list = glob.glob(os.path.join(behav_files_path, '*' + file_stub + '*'))
+    #import pdb; pdb.set_trace()
+
+    for f in file_list:
+        df_add = read_nki_cvs(f)
+        # if 'Subject Type' in df_add.columns:
+        #     print('Dropping Subject Type from df as this often causes duplicates.')
+        #     df_add = df_add.drop('Subject Type', axis=1)
+
+        df = merge_dataframes(df, df_add)
+
+
+#add subjects id mapping to df
+df = merge_dataframes(df, leica_subjects)
+
+#remove subjects that are not in leica
+df = df[~df.leica_id.isnull()]
+
+#remove exactly duplicated lines
+df = df[~df.duplicated()]
+
+#print ambigous duplicates (same index but different values
+print('DUPLICATE INDICES!!!')
+print(df.index.get_duplicates())
