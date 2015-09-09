474364313a3a8a543c44ca1ded094b1370611729
fliem <franz.liem>
2015-09-09 19:43:47 +0200

    works now

--- preprocessing/normalize.py
+++ preprocessing/normalize.py
@@ -1,10 +1,10 @@
 
-def normalize_epi(in_data,
-                  ref_data,
-                  warpfield,
-                  save_name,
+def normalize_epi(subjects_list,
+                  TR_list,
+                  preprocessed_data_dir,
                   working_dir,
                   ds_dir,
+                  template_dir,
                   plugin_name,
                   use_n_procs):
 
@@ -13,6 +13,7 @@ def normalize_epi(in_data,
     from nipype.pipeline.engine import Node, Workflow
     import nipype.interfaces.io as nio
     from nipype.interfaces import fsl
+    import nipype.interfaces.utility as util
 
 
     #####################################
@@ -28,24 +29,71 @@ def normalize_epi(in_data,
     wf.config['execution']['crashdump_dir'] = os.path.join(working_dir, 'crash')
 
 
-    ds = Node(nio.DataSink(base_directory=ds_dir), name='ds')
-    # ds.inputs.regexp_substitutions = [
-    #     ('subject_id_', '')]  # [('_variabilty_MNIspace_3mm[0-9]*/', ''), ('_z_score[0-9]*/', '')]
+    ds = Node(nio.DataSink(), name='ds')
+    ds.inputs.substitutions = [('_TR_id_', 'TR_')]
+    ds.inputs.regexp_substitutions = [('_subject_id_[A0-9]*/', '')]
+
+    #####################################
+    # SET ITERATORS
+    #####################################
+    # GET SCAN TR_ID ITERATOR
+    scan_infosource = Node(util.IdentityInterface(fields=['TR_id']), name='scan_infosource')
+    scan_infosource.iterables = ('TR_id', TR_list)
+
+    subjects_infosource = Node(util.IdentityInterface(fields=['subject_id']), name='subjects_infosource')
+    subjects_infosource.iterables = ('subject_id', subjects_list)
+
+    def add_subject_id_to_ds_dir_fct(subject_id, ds_path):
+        import os
+        out_path = os.path.join(ds_path, subject_id)
+        return out_path
+
+    add_subject_id_to_ds_dir = Node(util.Function(input_names=['subject_id', 'ds_path'],
+                                                  output_names=['out_path'],
+                                                  function=add_subject_id_to_ds_dir_fct),
+                                    name='add_subject_id_to_ds_dir')
+    wf.connect(subjects_infosource, 'subject_id', add_subject_id_to_ds_dir, 'subject_id')
+    add_subject_id_to_ds_dir.inputs.ds_path = ds_dir
+
+    wf.connect(add_subject_id_to_ds_dir, 'out_path', ds, 'base_directory')
+
+
+
+   # get atlas data
+    templates_atlases = {'FSL_MNI_3mm_template': 'MNI152_T1_3mm_brain.nii.gz',
+                         }
+
+    selectfiles_anat_templates = Node(nio.SelectFiles(templates_atlases,
+                                                      base_directory=template_dir),
+                                      name="selectfiles_anat_templates")
+
+    # GET SUBJECT SPECIFIC FUNCTIONAL AND STRUCTURAL DATA
+    selectfiles_templates = {
+        'epi_2_MNI_warp': '{subject_id}/rsfMRI_preprocessing/registration/epi_2_MNI_warp/TR_{TR_id}/*.nii.gz',
+        'preproc_epi_full_spectrum': '{subject_id}/rsfMRI_preprocessing/epis/01_denoised/TR_{TR_id}/*.nii.gz',
+        'preproc_epi_bp': '{subject_id}/rsfMRI_preprocessing/epis/02_denoised_BP/TR_{TR_id}/*.nii.gz',
+        'preproc_epi_bp_tNorm': '{subject_id}/rsfMRI_preprocessing/epis/03_denoised_BP_tNorm/TR_{TR_id}/*.nii.gz',
+    }
+
+    selectfiles = Node(nio.SelectFiles(selectfiles_templates,
+                                       base_directory=preprocessed_data_dir),
+                       name="selectfiles")
+    wf.connect(scan_infosource, 'TR_id', selectfiles, 'TR_id')
+    wf.connect(subjects_infosource, 'subject_id', selectfiles, 'subject_id')
+
 
 
     # fixme
     # CREATE TS IN MNI SPACE
-    # is it ok to apply the 2mm warpfield to the 3mm template?
-    # seems ok: https://www.jiscmail.ac.uk/cgi-bin/webadmin?A2=ind0904&L=FSL&P=R14011&1=FSL&9=A&J=on&d=No+Match%3BMatch%3BMatches&z=4
     epi_MNI = Node(fsl.ApplyWarp(), name='epi_MNI')
     epi_MNI.inputs.interp = 'spline'
-    epi_MNI.plugin_args = {'submit_specs': 'request_memory = 4000'} #{'dagman_args': 'request_memory = 4000'}
-    epi_MNI.inputs.in_file = in_data
-    epi_MNI.inputs.ref_file = fsl.Info.standard_image(ref_data)
-    epi_MNI.inputs.field_file = warpfield
+    epi_MNI.plugin_args = {'submit_specs': 'request_memory = 4000'}
+    wf.connect(selectfiles, 'preproc_epi_full_spectrum', epi_MNI, 'in_file')
+    wf.connect(selectfiles, 'epi_2_MNI_warp', epi_MNI, 'field_file')
+    wf.connect(selectfiles_anat_templates, 'FSL_MNI_3mm_template', epi_MNI, 'ref_file')
+    epi_MNI.inputs.out_file = 'preprocessed_fullspectrum_MNI_3mm.nii.gz'
 
-
-    wf.connect(epi_MNI, 'out_file', ds, save_name)
+    wf.connect(epi_MNI, 'out_file', ds, 'rsfMRI_preprocessing.epis_MNI_3mm.01_denoised')
 
 
     #####################################
--- run_15_normalize_epis.py
+++ run_15_normalize_epis.py
@@ -4,47 +4,40 @@ how to run
 
 '''
 
+# fixme
+# some (all?) already have /scr/adenauer2/Franz/LeiCA_NKI_test/c/0120659/rsfMRI_preprocessing/epis_MNI_3mm
+# from centrality calc
+
 import os
 
 # # LeiCA modules
 from preprocessing import normalize
 
-from variables import working_dir, ds_dir, preprocessed_data_dir
-from variables import subjects_list
-from variables import use_n_procs, plugin_name
+from variables import working_dir, ds_dir, preprocessed_data_dir, template_dir
+from variables import full_subjects_list, TR_list
+#from variables import use_n_procs, plugin_name
 
 
 ##############################
-#fixme
-working_dir = '/scr/adenauer2/Franz/LeiCA_NKI_test/ws'
-ds_dir = '/scr/adenauer2/Franz/LeiCA_NKI_test/ds'
-subjects_list = subjects_list[0:1]
+
+working_dir = '/scr/adenauer1/Franz/LeiCA_NKI/normalize'
+
 ##############################
 
-working_dir = os.path.join(working_dir, 'normalize')
+use_n_procs = 30
+plugin_name = 'MultiProc'
 
+# fixme
+# ignore warning from np.rank
+import warnings
 
-
-
-for subject_id in subjects_list:
-    subject_working_dir = os.path.join(working_dir, subject_id)
-    subject_ds_dir = os.path.join(ds_dir, subject_id)
-
-    subject_in_data = os.path.join(preprocessed_data_dir,
-                                   '%s/rsfMRI_preprocessing/epis/01_denoised/TR_645/residual.nii.gz' % subject_id)
-    subject_warpfield = os.path.join(preprocessed_data_dir,
-                                     '%s/rsfMRI_preprocessing//registration/epi_2_MNI_warp/TR_645/MNI152_T1_2mm_concatwarp.nii.gz' % subject_id)
-
-    save_name = 'epis.11_denoised_MNI.TR_645.preprocessed_fullspectrum_MNI_2mm.nii.gz'
-
-    fsl_template = 'MNI152_T1_2mm_brain.nii.gz'
-    # INPUT PARAMETERS for pipeline
-
-normalize.normalize_epi(in_data=subject_in_data,
-                        ref_data=fsl_template,
-                        warpfield=subject_warpfield,
-                        save_name=save_name,
-                        working_dir=subject_working_dir,
-                        ds_dir=subject_ds_dir,
-                        plugin_name=plugin_name,
-                        use_n_procs=use_n_procs)
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    normalize.normalize_epi(subjects_list=full_subjects_list,
+                            TR_list=TR_list,
+                            preprocessed_data_dir=preprocessed_data_dir,
+                            working_dir=working_dir,
+                            ds_dir=ds_dir,
+                            template_dir=template_dir,
+                            plugin_name=plugin_name,
+                            use_n_procs=use_n_procs)
